931db7b5f207cee76a7a73ea18152cd2fd2c91ae
ClothAtTheFair <bradleybsf12@gmail.com>
2020-03-31 22:41:31 -0400

    init commit, countdown is working but need to implement other functions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,129 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+let countdown = 0;
+let seconds = 1500;
+
+let isBreak = true;
+let isPaused = true;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    // placeholder
+    this.state = {
+      workMin: 25,
+      workSec: 0,
+      breakMin: 10, 
+      breakSec: 0
+    }
+  }
+
+  
+
+  componentDidMount(){
+    // not sure how to tell from different buttons
+    this.handleWorkPlus = this.handleWorkPlus.bind(this);
+    this.handleStart = this.handleStart.bind(this);
+  }
+
+  handleStart = e => {
+    e.preventDefault();
+    this.myInterval = setInterval(() => {
+      const {workSec, workMin} = this.state
+ 
+      if (workSec > 0) {
+        this.setState(({workSec}) => ({
+          workSec: workSec-1
+        }))
+      }
+      if (workSec === 0) {
+        if (workMin === 0) {
+          clearInterval(this.myInterval)
+        } else {
+          this.setState(({workMin}) => ({
+            workMin: workMin -1,
+            workSec: 59
+          }))
+        }
+      }
+    }, 1000)
+  }
+  // This acts like a pause button currently
+  handleReset = e => {
+    e.preventDefault();
+    clearInterval(this.myInterval)
+  }
+
+ 
+
+  handleWorkPlus = e => {
+    const {workMin} = this.state;
+    e.preventDefault();
+    this.setState({
+      workMin: workMin + 1,
+    });
+  }
+
+  handleWorkMinus = e => {
+   const {workMin} = this.state
+    e.preventDefault();
+    if (this.state.workMin > 1) {
+      this.setState({
+        workMin: workMin - 1
+      });
+    }    
+  }
+
+  handleBreakPlus = e => {
+    let breakcopy = this.state.breakMin;
+    e.preventDefault();
+    this.setState({
+      breakMin: breakcopy += 1
+    });
+  }
+
+  handleBreakMinus = e => {
+    let breakcopy = this.state.breakMin;
+    e.preventDefault();
+    if (this.state.breakMin > 1) {
+      this.setState({
+        breakMin: breakcopy -= 1
+      });
+    }    
+  }
+ 
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <h1>Under Construction</h1>
+      <div id="pomodoro">
+        <div id="status"></div>
+        <h4>Time Remaining: {this.state.workMin}:{this.state.workSec < 10 ? `0${this.state.workSec}` : this.state.workSec} </h4>
+        <button onClick={this.handleStart}>Start</button>
+      </div>
+      
+      <div class="settings">
+        <div id="work">
+          <p>Work Duration</p>
+          <button id="work-plus" onClick={this.handleWorkPlus}>+</button>
+          <div><span>{this.state.workMin}</span> mins</div>
+          <button id="work-minus" onClick={this.handleWorkMinus}>-</button>
+        </div>
+        
+        <button id="reset" class="btn" onClick={this.handleReset} >RESET</button>
+        
+        <div id="break">
+          <p>Break Duration</p>
+          <button id="break-plus" onClick={this.handleBreakPlus}>+</button>
+          <div><span id="break-min">{this.state.breakMin}</span> mins </div>
+          <button id="break-minus" onClick={this.handleBreakMinus}>-</button>
+        
+        </div>
+    </div>
     </div>
   );
 }
-
+}
 export default App;

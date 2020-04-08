8578155a76805eff1e11d4b9087e13a778eafece
ClothAtTheFair <bradleybsf12@gmail.com>
2020-04-08 00:46:39 -0400

    Added in a way to reset and the beginning of firebase integration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// TODO: -set up authorization with google or email
+// - have pomodoro's written to db connected by the auth username
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -13,6 +16,8 @@ class App extends Component {
       breakSec: 0,
       isBreak: false,
       disabled: false,
+      pomodoroThisMonth: 0,
+      allTimePomodoro: 0
     }
   }
 
@@ -60,7 +65,7 @@ class App extends Component {
   handleBreakStart = () => {
     clearInterval(this.myInterval)
     this.myInterval = setInterval(() => {
-    const {breakMin, breakSec} = this.state
+    const {breakMin, breakSec, allTimePomodoro} = this.state
       if (breakSec > 0) {
         this.setState(({breakSec}) => ({
           breakSec: breakSec-1
@@ -68,7 +73,12 @@ class App extends Component {
       }
       if (breakSec === 0) {
         if (breakMin === 0) {
+          this.setState ({
+            isBreak: false,
+            allTimePomodoro: allTimePomodoro + 1
+          });
           clearInterval(this.myInterval)
+          this.handleReset()
         } else {
           this.setState(({breakMin}) => ({
             breakMin: breakMin -1,
@@ -77,6 +87,16 @@ class App extends Component {
         }
       }
     }, 1000)
+  }
+
+  handleReset = () => {
+    clearInterval(this.myInterval);
+    this.setState ({
+      isBreak: false,
+      workMin: 25,
+      workSec: 0,
+      breakMin: 10
+    });
   }
  
 
@@ -121,33 +141,34 @@ render() {
   const isBreak = this.state.isBreak;
   return (
     <div className="container">
-    <h1>Under Construction</h1>
+    <h1 className = "title">Pomodoro Timer</h1>
       <div id="pomodoro">
         <div id="status"></div>
-        <h4>Yikes Forever</h4>
         { isBreak 
           ?<h4>Break Remaining: {this.state.breakMin}:{this.state.breakSec < 10 ? `0${this.state.breakSec}` : this.state.breakSec}</h4>
           : <h4>Time Remaining: {this.state.workMin}:{this.state.workSec < 10 ? `0${this.state.workSec}` : this.state.workSec} </h4>
         }
-        <button onClick={this.handleStart} disabled={this.state.disabled}>Start</button>
-        <button id="pause" class="btn" onClick={this.handlePause} >Pause</button>
+        <button className="button" onClick={this.handleStart} disabled={this.state.disabled}>Start</button>
+        <button id="pause" className="button"  onClick={this.handlePause} >Pause</button>
+        <button id="reset" className="button" onClick={this.handleReset}>Reset</button>
       </div>
       
       <div class="settings">
         <div id="work">
           <p>Work Duration</p>
-          <button id="work-plus" onClick={this.handleWorkPlus}>+</button>
+          <button id="work-plus" className="button"  onClick={this.handleWorkPlus} disabled={this.state.disabled}>+</button>
           <div><span>{this.state.workMin}</span> mins</div>
-          <button id="work-minus" onClick={this.handleWorkMinus}>-</button>
+          <button id="work-minus" className="button"  onClick={this.handleWorkMinus} disabled={this.state.disabled}>-</button>
         </div>
         
         <div id="break">
           <p>Break Duration</p>
-          <button id="break-plus" onClick={this.handleBreakPlus}>+</button>
+          <button id="break-plus" className="button"  onClick={this.handleBreakPlus}>+</button>
           <div><span id="break-min">{this.state.breakMin}</span> mins </div>
-          <button id="break-minus" onClick={this.handleBreakMinus}>-</button>
+          <button id="break-minus"  className="button" onClick={this.handleBreakMinus}>-</button>
         
         </div>
+        <h4>Pomodoro's completed all time: <span>{this.state.allTimePomodoro}</span></h4>
     </div>
     </div>
   );
